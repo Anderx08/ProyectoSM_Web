82d277f6aa7e1095aad113a0dcfd8a7bf4861fc2
Anderx08 <andeespi08@gmail.com>
2025-02-01 17:22:49 -0600

    Semana_02

---
+++ SM_ProyectoWEB/SM_ProyectoWEB/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SM_ProyectoWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult IniciarSesion()
+        {
+            return View();
+        }
+
+        public IActionResult Principal()
+        {
+            return View();
+        }
+
+        public IActionResult RegistrarCuenta() { 
+            return View();
+        }
+
+        public IActionResult RecuperarContra() {
+            return View();
+        }
+    }
+}
---
+++ SM_ProyectoWEB/SM_ProyectoWEB/Models/UsuarioModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SM_ProyectoWEB.Models
+{
+    public class UsuarioModel
+    {
+        public string Identificacion { get; set; }
+
+        public string Contrasena{ get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Correo { get; set; }
+    }
+}
